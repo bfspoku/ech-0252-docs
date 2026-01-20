460dfeb205ad9116f4365b4acc53e0a2ab8a024b
APatelli <alessandro.s.patelli@gmail.com>
2026-01-20 14:45:39 +0100

    Jenny's comments integrated

--- content/latest-news.js
+++ content/latest-news.js
@@ -19,11 +19,15 @@ var newsData = [
     { 
         "date": "20.01.2026", 
         "title": "The Swiss Election ETL System is up and running", 
-        "message": `The new server features an ETL system that enables automated file processing and testing for the 2027 Swiss Federal Election.
-        <br>
-        This setup implements also an automatic feedback report transferred by email that will help detecting strucural errors in the XML file for both Information and Results deliveries.
-        <br><br>
-        Moreover, we provide a <a href=guide.html>eCH-0252 implementation guide</a> dedicated to Federal Election, <a href=yaml_file.html>YAML files</a> containing mandatory XML structures and <a href=xml_file.html>XML examples</a> for both information and results deliveries. 
-        `
-    }
+        "message": `The Swiss Elections ETL system is now operational. It enables automated processing and validation of eCH-0252 XML files in preparation for the 2027 Swiss Federal Elections.
+                    <br><br>
+                    The system includes automated structural and schema-based checks for both information and result deliveries. Validation outcomes are compiled into a feedback report and sent by email to support early detection of format-related issues in the submitted deliveries.
+                    <br>
+                    In addition, the following supporting materials are available:
+                    <ul class="unstyled">
+                    <li> An eCH-0252 Implementation Guide specifically dedicated to Swiss Federal Elections </li>
+                    <li> YAML files defining mandatory XML structures as a baseline for implementation </li>
+                    <li> XML example files illustrating compliant information and results deliveries </li>
+                    </ul>`
+}
 ]
